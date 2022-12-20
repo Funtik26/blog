806c8e8fa86b4025591cad1ad9cf711b7b7cca6a
Funtik26 <80151464+Funtik26@users.noreply.github.com>
2022-12-20 20:23:26 +0300

    fix: initial blog settings
    Updated blog name, description

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Vlada Rusina blog on technical writing',
+  author: 'Vlada Rusina',
+  headerTitle: 'VladaRusina.com',
+  description: 'Vlada Rusina blog on technical writing',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
