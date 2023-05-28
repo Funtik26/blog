ea8c3a5ec574d7c4490d5e94208a76c4698d0edf
editor <vladarusina@gmail.com>
2023-05-28 10:36:31 +0300

    fix: ncu

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-
-import '@fontsource/inter/variable-full.css'
+// Supports weights 100-900
+import '@fontsource-variable/inter'
 
 // import { Analytics } from '@vercel/analytics/react'
 
