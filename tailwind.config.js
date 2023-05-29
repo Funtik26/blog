b7aed8e3bb4802f34ebac2639989519a263f6b5a
editor <vladarusina@gmail.com>
2023-05-29 19:59:49 +0300

    fix: update fontsource v5 usage

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         14: '3.5rem',
       },
       fontFamily: {
-        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: colors.teal,
