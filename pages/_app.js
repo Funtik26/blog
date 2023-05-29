902aeaf1f2365d0ae5e4acea3f09c162801821a2
editor <vladarusina@gmail.com>
2023-05-29 19:47:10 +0300

    fix: remove mailchimp, update README, bump fontsource to v5

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
 
-import '@fontsource/inter/variable-full.css'
+import '@fontsource-variable/inter'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
